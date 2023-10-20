296e7451ea0c5cc10a90b997a957c1a7cfa27df3
goutamoff7 <goutamoff7@gmail.com>
2023-10-20 10:05:54 +0530

    Overriding rules achieve Runtime Polymorphism modified

--- inheritance/InheritanceOverridingRules_RunTimePolymorphism.java
+++ inheritance/InheritanceOverridingRules_RunTimePolymorphism.java
@@ -4,7 +4,7 @@ class Animal {
 		System.out.println("Animal can run");
 	}
 
-	public void eat() // overrdden method
+	public void eat() // overridden method
 	{
 		System.out.println("Animal eats everyday");
 	}
@@ -14,12 +14,14 @@ class Animal {
 		System.out.println("Animal age is " + x);
 	}
 
-	private void sleep() // method with private specifier can't participate in inheritance
+	private void sleep() // method with private specifier can't participate
+							// in inheritance
 	{
 		System.out.println("Animal needs good sleep");
 	}
 
-	final void shelter() {
+	final void shelter() // can't override final method so it acts as inherited method
+	{
 		System.out.println("Animal needs shelter");
 	}
 
@@ -31,6 +33,11 @@ class Tiger extends Animal {
 		System.out.println("Tiger can run very fast");
 	}
 
+	public void eat() // overriding method
+	{
+		System.out.println("Tiger hunts and eat");
+	}
+
 	// public int age(int x) // The return type is incompatible with Animal.age(int)
 	// {
 	// return 10;
@@ -39,14 +46,11 @@ class Tiger extends Animal {
 	// void age(int x) // Cannot reduce the visibility of the inherited method from
 	// Animal (public to default)
 	// {
-	// System.out.println("Tiger hunts and eat");
+	// System.out.println("Tiger age: "+x);
 	// }
-	public void eat() // overriding method
-	{
-		System.out.println("Tiger hunts and eat");
-	}
 
-	public void age() // act as specialized method of Tiger class as argument changed
+	public void age() // act as specialized method of Tiger class as argument changed satisfies method
+						// overloading
 	{
 		System.out.println("Tiger class specialized method");
 	}
@@ -59,15 +63,15 @@ class Tiger extends Animal {
 
 public class InheritanceOverridingRules_RunTimePolymorphism {
 	public static void main(String[] args) {
-		Tiger t = new Tiger();
-		Animal a = new Tiger();
+		Tiger t = new Tiger(); // Child class reference manages child class object
+		Animal a = new Tiger(); // Parent class reference manages child class object
 
 		a.run();
 		t.run();
 		a.eat();
 		t.eat();
-		// a.age();//specialized method of base class can't call with reference of super
-		// class
+		// a.age();//specialized method of child class can't call with parent class
+		// reference variable
 		t.age();
 		a.age(5);
 		t.age(5);
