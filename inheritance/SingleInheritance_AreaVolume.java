5343b7d88e7d5726b2f105c48e294f62d842a8b7
goutamoff7 <goutamoff7@gmail.com>
2023-10-26 21:38:39 +0530

    more programs on inheritance

---
+++ inheritance/SingleInheritance_AreaVolume.java
@@ -0,0 +1,25 @@
+class Parent {
+	int length, breadth;
+
+	int area(int length, int breadth) {
+		this.length = length;
+		this.breadth = breadth;
+		return length * breadth;
+	}
+}
+
+class Child extends Parent {
+	int volume(int height) {
+		return length * breadth * height;
+	}
+}
+
+public class SingleInheritance_AreaVolume {
+	public static void main(String[] args) {
+		Child c = new Child();
+		int area = c.area(5, 6);
+		int volume = c.volume(3);
+		System.out.println("Area of the Square = " + area + " Sqr mtr");
+		System.out.println("Volume of the Square = " + volume + " Cubic mtr");
+	}
+}
