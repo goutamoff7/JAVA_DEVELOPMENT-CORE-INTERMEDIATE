2395698644e0c3f309759e00248e0a9e88a7608a
goutamoff7 <goutamoff7@gmail.com>
2023-12-22 23:33:37 +0530

    LeetCode Problem 26,1422

---
+++ LeetCodeProblems/Problem1422.java
@@ -0,0 +1,41 @@
+/*LeetCode problem 1422. Maximum Score After Splitting a String
+Input: s = "011101"
+Output: 5 
+Explanation: 
+All possible ways of splitting s into two non-empty substrings are:
+left = "0" and right = "11101", score = 1 + 4 = 5 
+left = "01" and right = "1101", score = 1 + 3 = 4 
+left = "011" and right = "101", score = 1 + 2 = 3 
+left = "0111" and right = "01", score = 1 + 1 = 2 
+left = "01110" and right = "1", score = 2 + 1 = 3*/
+ 
+class Problem1422 {
+        public int maxScore(String s) {
+            int max=0,count=0;
+            for(int i=0;i<s.length()-1;i++)
+            {
+                String left=s.substring(0,i+1);
+                for(int j=0;j<left.length();j++)
+                {
+                    if(left.charAt(j)=='0')
+                        count++;
+                }
+                String right=s.substring(i+1,s.length());
+                for(int k=0;k<right.length();k++)
+                {
+                    if(right.charAt(k)=='1')
+                        count++;
+                    
+                }
+		    System.out.println(count);
+                if(max<count)
+                    max=count;
+                count=0;
+            }
+            return max;
+        }
+        public static void main(String[] args) {
+            System.out.println("max= "+new Problem1422().maxScore("011101"));
+        }
+    }
+    
---
+++ LeetCodeProblems/Problem26.java
@@ -0,0 +1,24 @@
+/*LeetCode Problem 26. Remove Duplicates from Sorted Array
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).*/
+
+class Problem26 {
+    public int removeDuplicates(int[] nums) {
+        int i=0,insert=0;
+        while(i<nums.length)
+        {
+            if(nums[insert]!=nums[i])
+                nums[++insert]=nums[i];
+            i++;
+        }
+        for(int n=0;n<=insert;n++)
+        System.out.print(nums[n]+",");
+        System.out.println();
+        return insert+1  ;
+    }
+    public static void main(String[] args) {
+        System.out.println(new Problem26().removeDuplicates(new int[]{1,1,2,2,2,3}));
+    }
+}
