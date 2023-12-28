123a7789209fb3e32ba6acb6dd63f49c21a30c32
goutamoff7 <goutamoff7@gmail.com>
2023-12-28 23:26:47 +0530

    LeetCode Problem 2744 modified, adding Lambda Expression for Functional Interface

---
+++ interface/LambdaExp_FunctionalInterface.java
@@ -0,0 +1,54 @@
+interface A {
+	void m();
+}
+
+interface B {
+	void m(int a);
+}
+
+interface C {
+	void m(int a, int b);
+}
+/*
+ * If a interface has only one method (called Functional Interface)
+ * and a class which implements those method only once throughout the project
+ * then there
+ * is not a requirement for making a separate class we can use Lambda Expression
+ * 
+ * class B implements A {
+ * 
+ * public void m1()
+ * {
+ * System.out.println("Class B implements m1() method of interface 'A'");
+ * }
+ * }
+ */
+
+class LambdaExp_FunctionalInterface {
+	// implementation of the Single methods of interface 'A'
+	// through Lambda Expression.
+	// here Anonymous class file will not be generated
+	public static void main(String[] args) {
+
+		// no Parameter requires parenthesis "()"
+		A obA1 = () -> System.out.println("implements m() method of interface 'A'");
+		obA1.m();
+		A obA2 = () -> // multiple statement requires curly brace and semi colon "{ };"
+		{
+			System.out.println("implements m() method of interface 'A' 1st ");
+			System.out.println("implements m() method of interface 'A' 2nd ");
+		};
+		obA2.m();
+
+		B obB1 = (a) -> System.out.println("implements m() method of interface 'B'");
+		obB1.m(10);
+		B obB2 = x -> System.out.println("implements m() method of interface 'B'");
+		// for single parameter parenthesis are optional
+		obB2.m(10);
+
+		C obC = (x, y) -> System.out.println("implements m() method of interface 'C'");
+		// for more than one parameter parenthesis are mandatory
+		obC.m(10, 20);
+
+	}
+}
