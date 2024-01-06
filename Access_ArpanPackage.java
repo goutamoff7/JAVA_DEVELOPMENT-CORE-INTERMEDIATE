52c6175f8dac455d794dc6a52cf616247ef0ae96
goutamoff7 <goutamoff7@gmail.com>
2024-01-06 17:20:18 +0530

    Package Demonstration

---
+++ Access_ArpanPackage.java
@@ -0,0 +1,9 @@
+import arpanPackage.*;
+class Access_ArpanPackage
+{
+	public static void main(String args[])
+	{
+		A ob = new A("Hello Arpan");
+		ob.display();
+	}	
+}
---
+++ arpanPackage/A.java
@@ -0,0 +1,13 @@
+package arpanPackage;
+public class A
+{
+	String s;
+	public A(String s)
+	{
+		this.s=s;	
+	}
+	public void display()
+	{
+		System.out.println(s);	
+	}
+}
