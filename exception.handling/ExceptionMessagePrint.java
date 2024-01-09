b1e874e9c86e7f3e13f4d8e7ed0bb4d064816445
goutamoff7 <goutamoff7@gmail.com>
2024-01-09 12:13:31 +0530

    Various methods to print Exception Information

---
+++ exception.handling/ExceptionMessagePrint.java
@@ -0,0 +1,14 @@
+class ExceptionMessagePrint {
+	public static void main(String[] args) {
+		try{
+			System.out.println(10/0);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			System.out.println(e);
+			System.out.println(e.toString());
+			System.out.println(e.getMessage());
+		}
+	}
+}
