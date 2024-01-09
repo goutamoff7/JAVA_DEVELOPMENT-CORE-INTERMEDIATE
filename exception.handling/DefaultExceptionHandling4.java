561ad0aa032eb46179040683ce4881ee7422a0c4
goutamoff7 <goutamoff7@gmail.com>
2024-01-09 08:30:14 +0530

    Un-Handled Exception arise at method a(), then comes to caller main()

---
+++ exception.handling/DefaultExceptionHandling4.java
@@ -0,0 +1,19 @@
+//Demonstration for un-handled Exception
+public class DefaultExceptionHandling4 {
+	public static void main(String[] args) {
+		System.out.println("Entering Main method");
+		a();
+		System.out.println("Exiting Main method");
+	}
+	public static void a() {
+		System.out.println("Entering method a");
+		b();
+		System.out.println(10/0);
+		System.out.println("Exiting method a");
+	}
+	public static void b() {
+		System.out.println("Entering method b");
+		System.out.println("Hello");
+		System.out.println("Exiting method b");
+	}
+}
