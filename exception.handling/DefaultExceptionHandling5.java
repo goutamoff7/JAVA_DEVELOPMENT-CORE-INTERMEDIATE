6e6f1e3f823b93cd37c5a458c2a803dcb2f68bef
goutamoff7 <goutamoff7@gmail.com>
2024-01-09 08:31:15 +0530

    Un-Handled Exception arise at main()

---
+++ exception.handling/DefaultExceptionHandling5.java
@@ -0,0 +1,23 @@
+//Demonstration for un-handled Exception
+public class DefaultExceptionHandling5 {
+	public static void main(String[] args) {
+		System.out.println("Entering Main method");
+		a();
+		System.out.println(10/0);
+		System.out.println("Exiting Main method");
+	}
+
+	public static void a() {
+		System.out.println("Entering method a");
+		b();
+		System.out.println("Hiee");
+		System.out.println("Exiting method a");
+	}
+
+	public static void b() {
+		System.out.println("Entering method b");
+		System.out.println("Hello");
+		System.out.println("Exiting method b");
+	}
+
+}
