ea7e90c942f639ce80ce0966d0509860e56ee4ef
goutamoff7 <goutamoff7@gmail.com>
2024-01-10 11:28:26 +0530

    finally Block executes first then try block(Without Exception) or catch block (with exception) returned value

---
+++ exception.handling/Exception_finallyBlock_returnStatement1.java
@@ -0,0 +1,19 @@
+//Finally Block executes first then try block returned value (No Exception)
+
+public class Exception_finallyBlock_returnStatement1 {
+	public static int method() {
+		try {
+			System.out.println("try block executed");
+			return 5;
+		} catch (Exception e) {
+			System.out.println("catch block executed");
+			return 10;
+		} finally {
+			System.out.println("finally block executed");
+		}
+	}
+	public static void main(String[] args) {
+		System.out.println(method());
+	}
+
+}
---
+++ exception.handling/Exception_finallyBlock_returnStatement2.java
@@ -0,0 +1,21 @@
+//Finally Block executes first then catch block returned value (with Exception)
+
+public class Exception_finallyBlock_returnStatement2 {
+	public static int method() {
+		try {
+			System.out.println("try block executed");
+			System.out.println(10 / 0);
+			return 5;
+		} catch (Exception e) {
+			System.out.println("catch block executed");
+			return 10;
+		} finally {
+			System.out.println("finally block executed");
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println(method());
+	}
+
+}
