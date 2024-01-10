4bf5ddbf01845d182d1c087a2dc2e096da5a1cfb
goutamoff7 <goutamoff7@gmail.com>
2024-01-10 11:30:05 +0530

    finally Block overrides the value returned by try blocks (No Exception) or returned by catch block (With Exception)

---
+++ exception.handling/Exception_finallyBlock_returnStatement3.java
@@ -0,0 +1,21 @@
+//Finally Block overrides the value returned by try blocks (No Exception)
+
+public class Exception_finallyBlock_returnStatement3 {
+	public static int method() {
+		try {
+			System.out.println("try block executed");
+			return 5;
+		} catch (Exception e) {
+			System.out.println("catch block executed");
+			return 10;
+		} finally {
+			System.out.println("finally block executed");
+			return 20;
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println(method());
+	}
+
+}
---
+++ exception.handling/Exception_finallyBlock_returnStatement4.java
@@ -0,0 +1,22 @@
+//Finally Block overrides the value returned by catch blocks (With Exception)
+
+public class Exception_finallyBlock_returnStatement4 {
+	public static int method() {
+		try {
+			System.out.println("try block executed");
+			System.out.println(10 / 0);
+			return 5;
+		} catch (Exception e) {
+			System.out.println("catch block executed");
+			return 10;
+		} finally {
+			System.out.println("finally block executed");
+			return 20;
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println(method());
+	}
+
+}
