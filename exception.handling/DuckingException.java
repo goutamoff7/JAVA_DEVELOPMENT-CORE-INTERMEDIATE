21acf5fe92c7b493f731a5b16a923ebac5e776a2
goutamoff7 <goutamoff7@gmail.com>
2024-01-11 23:10:21 +0530

    Demonstration of Ducking an exception

---
+++ exception.handling/DuckingException.java
@@ -0,0 +1,21 @@
+public class DuckingException {
+	public static void a() throws ArithmeticException {
+		System.out.println(10 / 0);
+	}
+
+	public static void b() throws ArithmeticException {
+		System.out.println(20 / 0);
+	}
+
+	public static void main(String[] args) {
+		try{
+			a();
+			b();
+			}
+			catch(Exception e)
+			{
+				System.out.println("Error : "+e.getMessage());
+			}
+				
+	}
+}
