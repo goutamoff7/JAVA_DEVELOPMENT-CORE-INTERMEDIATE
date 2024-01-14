a7ce3e68b78f1e39466b2462088c47ab42ed96aa
goutamoff7 <goutamoff7@gmail.com>
2024-01-14 12:09:11 +0530

    Checked Exception must be handled

---
+++ exception.handling/Checked_ExceptionHandling3.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Checked_ExceptionHandling3 {
+	public static void main(String[] args) {
+		PrintWriter pw = new PrintWriter("abc.txt");
+		pw.println("Hello World");
+
+	}
+}
