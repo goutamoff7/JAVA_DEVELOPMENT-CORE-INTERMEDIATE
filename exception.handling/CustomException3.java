38cb602ab9e6d18e558a6ed466fab148256fa85b
goutamoff7 <goutamoff7@gmail.com>
2024-01-15 18:59:51 +0530

    we can't only throw custom exception object of fully checked, Partialchecked and checked Exception class, it must be caught by try-catch or declared to be thrown by throws keyword

---
+++ exception.handling/CustomException3.java
@@ -0,0 +1,12 @@
+public class CustomException3 {
+	public static void main(String[] args) {
+		throw new Exception();
+	}
+}
+// error: unreported exception Exception; must be caught or declared to be
+// thrown
+// this compilation error occurs because some child class of Exception class are
+// belongs to checked exception like IOException etc which must be
+// caught/handled by
+// try-catch
+// or need to be thrown using throws keyword
