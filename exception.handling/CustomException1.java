ac1b6946b10d2c559b4e026e0d0554e4d705491c
goutamoff7 <goutamoff7@gmail.com>
2024-01-15 18:44:54 +0530

    CustomException1 class extends RuntimeException class and throws CustomException1 object using throws keyword

---
+++ exception.handling/CustomException1.java
@@ -0,0 +1,9 @@
+public class CustomException1 extends RuntimeException {
+	public static void main(String[] args) throws CustomException1 {
+	}
+}
+// if CustomException1 class does not extend Throwable class or any sub class of
+// it like RuntimeException
+// class the we cant use throws CustomException1 it will cause
+// Compilation error:No exception of type CustomException1 can be thrown;
+// an exception type must be a subclass of ThrowableJava(16777536)
