b9c5112d4187cb4c5138e048522d654d2c083584
goutamoff7 <goutamoff7@gmail.com>
2024-01-17 10:13:47 +0530

    Default exception in both try and finally

---
+++ exception.handling/DefaultException_both_try_finally.java
@@ -0,0 +1,19 @@
+class DefaultException_both_try_finally {
+	public static void main(String[] args) {
+		System.out.println("Enter main method");
+		try {
+			System.out.println("Enter try block");
+			System.out.println(10 / 0);
+			System.out.println("Exit try block");
+		} catch (ArithmeticException e) {
+			System.out.println("Enter catch block");
+			System.out.println("Exit catch block");
+		} finally {
+			System.out.println("Enter finally block");
+			System.out.println(30 / 0);
+			System.out.println("Exit finally block");
+		}
+		System.out.println("Exit main method ");
+
+	}
+}
