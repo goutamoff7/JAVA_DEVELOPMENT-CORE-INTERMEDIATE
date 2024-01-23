2bdbc1f01424a607eab2a4ffee9b34f0bb534e1b
goutamoff7 <goutamoff7@gmail.com>
2024-01-23 12:18:32 +0530

    Demonstration of BigInteger class used to do mathematical operations which involve Integer calculations that are outside the long data type also

---
+++ basic/BigIntegerClassDemo.java
@@ -0,0 +1,23 @@
+import java.math.BigInteger;
+
+class BigIntegerClassDemo {
+	public static void main(String[] args) {
+		BigInteger c, d, e, f;
+		BigInteger[] g;
+		BigInteger a = new BigInteger("110");
+		BigInteger b = new BigInteger("25");
+		c = a.add(b);
+		d = a.subtract(b);
+		e = a.multiply(b);
+		f = a.divide(b);
+		g = a.divideAndRemainder(b);
+
+		System.out.println("Addition : " + c);
+		System.out.println("Subtraction : " + d);
+		System.out.println("Multiplication : " + e);
+		System.out.println("Division : " + f);
+		for (BigInteger n : g)
+			System.out.print(n + ",");
+
+	}
+}
