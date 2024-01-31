916da84d9eef6376fef28efde15a602a012ccfec
goutamoff7 <goutamoff7@gmail.com>
2024-01-31 12:32:13 +0530

    TreeSet Demonstration of Collection

---
+++ collectionFramework/Collection_TreeSet.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+class Collection_TreeSet {
+	public static void main(String[] args) {
+		TreeSet ts1 = new TreeSet();
+		// ts1.add(10.5); // adding any type of data in ts1 except integer,
+		// cause runtime exception. we can add only integer type data
+		ts1.add(100); // adding int in ts1
+		ts1.add(50); // adding int in ts1
+		ts1.add(150); // adding int in ts1
+		ts1.add(25); // adding int in ts1
+		ts1.add(75); // adding int in ts1
+		ts1.add(125); // adding int in ts1
+		ts1.add(175); // adding int in ts1
+
+		System.out.println(ts1);
+		// Priority Queue in java follow Binary Search Tree in DSA,
+		// parent value is greater than left child value and less than right child value
+		// elements are shown as In-Order-Traversal (Left-Value-Right) while print.
+		TreeSet ts2 = new TreeSet();
+		ts2.addAll(ts1); // add all the elements of ts1 in ts2
+		// ts2.add(2, 20); // index based addition not allowed
+		ts2.add(150);// Duplicate value if entered will not be included in TreeSet.
+		System.out.println(ts2);
+		System.out.println(ts2.contains(100)); // searching the vlaue in the Priority Queue,
+		System.out.println(ts2.contains(200));// if contains then return true elde false
+
+		System.out.println("higher of 50 : " + ts2.higher(50));
+		System.out.println("lower of 50 : " + ts2.lower(50));
+
+		System.out.println("ceiling of 50 : " + ts2.ceiling(50));
+		System.out.println("floor of 50 : " + ts2.floor(50));
+
+		System.out.println("higher of 40 : " + ts2.higher(40));
+		System.out.println("lower of 40 : " + ts2.lower(40));
+
+		System.out.println("ceiling of 40 : " + ts2.ceiling(40));
+		System.out.println("floor of 40 : " + ts2.floor(40));
+
+		System.out.println(ts2.first()); // In-Order-Traversal First value
+		System.out.println(ts2.last()); // In-Order-Traversal Last value
+
+		System.out.println(ts1);
+		ts1.remove(150);
+		System.out.println(ts1);
+
+		System.out.println(ts1.descendingSet()); // descending order not reflected to original TreeSet
+		System.out.println(ts1);
+
+		ts1.clear(); // removing all the elements permanently from the TreeSet
+		System.out.println(ts1);
+	}
+}
