7524cd2aa92a86f4dc2864b95c67487bd577a364
goutamoff7 <goutamoff7@gmail.com>
2024-02-07 21:17:10 +0530

    m

--- collectionFramework/Collection_ArrayDeque.java
+++ collectionFramework/Collection_ArrayDeque.java
@@ -24,8 +24,8 @@ class Collection_ArrayDeque {
 		System.out.println(ad1.contains(200));// if contains then return true else false
 
 		System.out.println("Size of the ArrayDeque : " + ad2.size());
-		ad2.remove(100); // remove first 100 only
-		ad2.remove(); // remove first element only
+		System.out.println(ad2.remove(100)); // remove first 100 only
+		System.out.println(ad2.remove()); // remove and return first element only
 		ad2.removeAll(ad1); // remove all the ad1 elements from ad2.
 		System.out.println(ad1);
 		System.out.println(ad2);
