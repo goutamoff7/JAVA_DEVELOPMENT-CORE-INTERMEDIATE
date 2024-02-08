185edafc24c792db2ffa2a9fb0c6ec8f7ebdb868
goutamoff7 <goutamoff7@gmail.com>
2024-02-08 11:13:00 +0530

    more methods added

--- collectionFramework/Collection_ArrayDeque.java
+++ collectionFramework/Collection_ArrayDeque.java
@@ -3,6 +3,7 @@ import java.util.*;
 class Collection_ArrayDeque {
 	public static void main(String[] args) {
 		ArrayDeque ad1 = new ArrayDeque();
+		// ad1.add(null); // null value cause NullPointerException
 		ad1.add(100); // adding integer in ad1
 		ad1.add(100); // adding integer in ad1
 		ad1.add(13.5f); // adding float in ad1
@@ -21,6 +22,7 @@ class Collection_ArrayDeque {
 		ad2.offerLast(634); // offering value at last position, it may be added or can't be added.
 		System.out.println(ad2);
 
+		// ad2.get(3);// get(index) method not exist,getting value via index not allowed
 		System.out.println("getFirst of ad2 : " + ad2.getFirst()); // Returns the first element of deque.
 		System.out.println("getLast of ad2 : " + ad2.getLast()); // Returns the last element of deque.
 
@@ -37,6 +39,7 @@ class Collection_ArrayDeque {
 		System.out.println("Last Element of ad2 peekLast() method : " + ad2.peekLast());
 		// Retrieves, but does not remove, the last element of this deque, or returns
 		// null if this deque is empty.
+		System.out.println(ad2);
 		System.out.println(ad2.poll()); // Retrieves and removes the head of the queue represented by this deque (in
 		// other words, the first element of this deque), or returns null if this deque
 		// is empty.
@@ -48,7 +51,7 @@ class Collection_ArrayDeque {
 		System.out.println("Last Element of ad2 pollLast() method : " + ad2.pollLast());
 		// Retrieves and removes the last element of this deque, or returns null if this
 		// deque is empty.
-
+		System.out.println(ad2);
 		ad2.pop(); // removes the first element from the stack represented by this deque.
 		System.out.println("After pop() method called : " + ad2);
 		ad2.push(58); // Pushes 58 at first position of the stack represented by thisdeque.
@@ -69,13 +72,20 @@ class Collection_ArrayDeque {
 		// Removes and returns the first element from this deque.
 		System.out.println("removeLast() method called : " + ad2.removeLast());
 		// Retrieves and removes the last element of this deque.
-		System.out.println("removeFirstOccurrence(20) method called of ll2 : " + ad2.removeFirstOccurrence(20));
+		System.out.println("removeFirstOccurrence(100) method called of ad1 : " + ad1.removeFirstOccurrence(100));
 		// Removes the first occurrence of the specified element in this deque (when
 		// traversing the deque from head to tail).
-		System.out.println("removeLastOccurrence(20) method called : " + ad2.removeLastOccurrence(20));
+		System.out.println("removeLastOccurrence(1000) method called of ad1 : " + ad1.removeLastOccurrence(100));
 		// Removes the last occurrence of the specified element in this deque (when
 		// traversing the deque from head to tail).
-		ad2.removeAll(ad1); // remove all the ad1 elements from ad2.
+
+		System.out.println(ad1);
+		System.out.println(ad2);
+		System.out.println("After retainAll : " + ad2.retainAll(ad1));
+		// keep those elements of ad2 which matches ad1 otherwise removes from ad2
+		System.out.println(ad1);
+		System.out.println(ad2);
+		ad2.removeAll(ad1); // remove adl the ad1 elements from ad2.
 		System.out.println(ad1);
 		System.out.println(ad2);
 		ad2.clear(); // delete all element of ad2
