1911830b5e563f9ea7868ca5d5ef4fcb9ab371e9
goutamoff7 <goutamoff7@gmail.com>
2024-02-22 18:54:33 +0530

    Leaked resource must be closed through finally block

---
+++ exception.handling/Exception_finallyBlock_ResourceClose.java
@@ -0,0 +1,22 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Exception_finallyBlock_ResourceClose {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a Number : ");
+		int n=0;
+		try{
+			n = sc.nextInt();
+		}
+		catch(InputMismatchException e)	
+		{
+			System.out.println("Enter a Number only");
+		}
+		finally{
+			sc.close();
+		}
+		System.out.println("Given Number : " + n);
+
+	}
+}
