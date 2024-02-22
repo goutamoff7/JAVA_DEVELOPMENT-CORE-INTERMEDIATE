36a4f108ca823a671e966e1fe317a665bd924a36
goutamoff7 <goutamoff7@gmail.com>
2024-02-22 18:55:31 +0530

    Demonstration of two try catch block

---
+++ exception.handling/Exception_Two_tryCatch.java
@@ -0,0 +1,14 @@
+public class Exception_Two_tryCatch {
+	public static void main(String[] args) {
+		try {
+			System.out.println(10 / 0);
+		} catch (ArithmeticException e) {
+			System.out.println("Divide by Zero error");
+		}
+		try {
+			System.out.println(args[0]);
+		} finally {
+			System.out.println("Command line argument should be passed");
+		}
+	}
+}
