50a7d10e4a0cc451bab9c693587e06dd94c410ad
goutamoff7 <goutamoff7@gmail.com>
2024-02-26 23:10:37 +0530

    Demonstration of BufferedReader class along with FileReader class

---
+++ fileHandling/BufferedReader_Demo.java
@@ -0,0 +1,25 @@
+import java.io.*;
+public class BufferedReader_Demo {
+    public static void main(String[] args)  {
+        
+        try
+        {         
+            File file = new File("Files","Goutam.txt");  
+            try (FileReader fr = new FileReader(file);BufferedReader br = new BufferedReader(fr)) 
+            {
+                for(String line=br.readLine();line!=null;line=br.readLine())
+                {
+                    System.out.println(line);
+                    
+                }
+            }
+
+        }
+        catch(IOException e)
+        {
+            System.out.println("Goutam.txt file not found");            
+        }
+
+        
+    }
+}
