336633cc03d7e37a94d0a1e96393b7d2b86cd92d
goutamoff7 <goutamoff7@gmail.com>
2024-02-26 23:07:45 +0530

    Demonstration of FileReader class

---
+++ fileHandling/FileReader_Demo1.java
@@ -0,0 +1,24 @@
+import java.io.*;
+public class FileReader_Demo1 {
+    public static void main(String[] args)  {
+        
+        try
+        {         
+            File file = new File("Files","Goutam.txt");  
+            try (FileReader fr = new FileReader(file)) {
+                for(int i=fr.read();i!=-1;i=fr.read())
+                {
+                    System.out.print((char)i);
+                    
+                }
+            }
+
+        }
+        catch(IOException e)
+        {
+            System.out.println("Goutam.txt file not found");            
+        }
+
+        
+    }
+}
---
+++ fileHandling/FileReader_Demo2.java
@@ -0,0 +1,23 @@
+import java.io.*;
+public class FileReader_Demo2 {
+    public static void main(String[] args)  {
+        
+        try
+        {         
+            File file = new File("Files","Goutam.txt");  
+            try (FileReader fr = new FileReader(file)) {
+                char ar[]= new char[(int)file.length()];
+                fr.read(ar);
+                for(char data:ar)
+                    System.out.print((char)data);      
+            }
+
+        }
+        catch(IOException e)
+        {
+            System.out.println("Goutam.txt file not found");            
+        }
+
+        
+    }
+}
