efbd27fe356d02c31bd1ff2501b2e52d111d62e1
goutamoff7 <goutamoff7@gmail.com>
2024-02-28 23:35:23 +0530

    Writing on a text file using FileWriter and BufferedWriter

---
+++ fileHandling/FileWriter_BufferedWriter.java
@@ -0,0 +1,20 @@
+import java.io.*;
+class FileWriter_BufferedWriter
+{
+    public static void main(String[] args) throws Exception{
+        File dir = new File("Folder");
+        if(!dir.isDirectory())
+             dir.mkdir();
+        File file = new File(dir,"Demo.txt");
+        if(!file.isFile())    
+            file.createNewFile();
+        BufferedWriter  bw = new BufferedWriter(new FileWriter(file,true));
+        bw.write("Hello Programmers this is the text file to demonstrate the FileHandling");
+        bw.newLine();
+        bw.write("Good Bye");
+
+        bw.flush();
+        bw.close();        
+        
+    }
+}
