8e949b6736f6d68dad5c20e3ee177ee08418abdc
goutamoff7 <goutamoff7@gmail.com>
2024-03-09 23:19:42 +0530

    Demonstration of map method of Stream class which implements apply method of Function interface

---
+++ streamAPI/StreamAPI_map_Function_apply.java
@@ -0,0 +1,34 @@
+package streamAPI;
+import java.util.List;
+import java.util.stream.Stream;
+import java.util.Arrays;
+import java.util.function.Function;
+
+public class StreamAPI_map_Function_apply {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(7,6,3,5,10,9,8);
+        System.out.println(list);
+
+        Stream<Integer> str = list.stream();
+        
+        //implement apply method of Funtion Interface
+        // Function<Integer,Integer> fun = new Function<Integer,Integer>() {
+            
+        //         @Override   
+        //         public Integer apply(Integer n)
+        //         {
+        //             return n*n;
+        //         }
+            
+        // };  
+        // Stream<Integer> mapped = str.map(fun);  // Passing Funtion as argument in map method
+        
+        //Implement using lambda expression , similar work in one line
+        Stream<Integer> mapped =str.map(n->n*n);
+        
+        mapped.forEach(n->System.out.print(n+", "));
+
+
+    }
+    
+}
