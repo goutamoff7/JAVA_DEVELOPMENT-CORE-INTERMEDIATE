0491c0c83ceb3146dad08ebab6853f12b06c35ec
goutamoff7 <goutamoff7@gmail.com>
2024-03-15 21:26:11 +0530

    every declared enum internally extends java.lang.Enum

--- enumDemo/Enum_Demo.java
+++ enumDemo/Enum_Demo.java
@@ -1,15 +1,26 @@
 package enumDemo;
 import java.lang.Enum; //in-built Enum abstract class in java
 class Enum_Demo {
-    //Java enums are implicitly final subclasses of java.lang.Enum class
+    //Java enums are implicitly extends of java.lang.Enum class
     // enumerator used to define a group of named constants, convention : CAPITAL LETTERS
+
    enum Direction  
    {
         NORTH,SOUTH,EAST,WEST; 
    } 
+
+//behind the scene : 
+/*public final class Direction extends java.lang.Enum
+*{
+*   public static final Direction EAST = new Direction();
+*   public static final Direction WEST = new Direction();
+*   public static final Direction NORTH = new Direction();
+*   public static final Direction SOUTH = new Direction();
+}*/
    public static void main(String[] args) {
     
-    enum Result 
+    // enums can be declared inside the method body //
+    enum Result  
     {
         PASS,FAIL;
     }
