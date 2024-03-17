b52d9ac7912c1557b98590ee5ebb3780940e7ace
goutamoff7 <goutamoff7@gmail.com>
2024-03-17 09:06:25 +0530

    Application of custom made Annotation

--- annotationDemo/CustomAnnotation_Demo1.java
+++ annotationDemo/CustomAnnotation_Demo1.java
@@ -5,6 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+
 //lets make an custom annotation to embed additional details to Goutam class
 @Retention(RetentionPolicy.RUNTIME) //refers to the runtime, available to java compiler and JVM .
 @Target({ElementType.CONSTRUCTOR, ElementType.FIELD, ElementType.LOCAL_VARIABLE, ElementType.METHOD, ElementType.PACKAGE, ElementType.PARAMETER, ElementType.TYPE,ElementType.ANNOTATION_TYPE}) 
@@ -12,23 +13,23 @@ import java.lang.annotation.Target;
 //TYPE : Class, interface (including annotation interface)
 //PARAMETER : Formal parameter declaration
 //ANNOTATION_TYPE : Annotation interface declaration (Formerly known as an annotation type.
-@interface StudentDetails 
+@interface StudentDetails  // Multi-Valued Annotation
 {
     String college();
     String qualification() default "Under Graduate"; // no need to give value for qualification, by default it will set
 }
 
-@StudentDetails (college="Sanaka")   // Annotation used TYPE: interface 
-interface CustomAnnotation
+@StudentDetails (college="Sanaka")    
+interface CustomAnnotation     // Annotation used in TYPE: interface
 {
 
 }
 
-@StudentDetails (college="Sanaka")// Annotation used in ANNOTATION_TYPE : annotation interface
+@StudentDetails (college="Sanaka")
 @Retention(RetentionPolicy.SOURCE) //refers to the source code, discarded during compilation. It will not be available in the compiled class.
 @Target(ElementType.TYPE)
-@interface TestAnnotation  // Marker Annotation
-{ } 
+@interface TestAnnotation { }   // Annotation used in ANNOTATION_TYPE : annotation interface, Marker Annotation
+
 
 @Retention(RetentionPolicy.CLASS) //refers to the .class file, available to java compiler but not to JVM . It is included in the class file.
 @Target(ElementType.TYPE)
@@ -37,13 +38,13 @@ interface CustomAnnotation
     int[] a() default {10,20}; // 1-D Array allowed
 }
 
-//Applying annotation @TestAnnotation2
+//Applying the annotation
 @TestAnnotation2
 class A{}
 
-//Applying annotation @StudentDetails
-@StudentDetails (college="Sanaka") // Annotation used in TYPE : class 
-class Goutam // Multi-Valued Annotation
+//Applying annotation
+@StudentDetails (college="WBSI",qualification="Diploma") // Annotation used in TYPE : class 
+class Goutam 
 {
     
     String name;
