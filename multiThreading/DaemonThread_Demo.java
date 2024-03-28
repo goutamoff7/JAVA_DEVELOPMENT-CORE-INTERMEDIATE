43b2f0bae56565ed88d991d9b0ddf8df03292048
goutamoff7 <goutamoff7@gmail.com>
2024-03-28 20:16:24 +0530

    Demonstration of Daemon Thread in java, provide service to non-Daemon thread

---
+++ multiThreading/DaemonThread_Demo.java
@@ -0,0 +1,27 @@
+package multiThreading;
+class Daemon implements Runnable
+{
+    public void run()
+    {
+        if(Thread.currentThread().isDaemon())
+            System.out.println("Daemon Thread");
+        else
+            System.out.println("Child Thread");
+    }
+}
+class DaemonThread_Case
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Main Thread");
+        
+        Daemon d1 = new Daemon();
+        Thread t = new Thread(d1);
+
+        t.setDaemon(true); 
+        t.start(); 
+         
+
+
+    }
+}
