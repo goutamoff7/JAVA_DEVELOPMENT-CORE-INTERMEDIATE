79582c0018af225ec14393894d3ef09e93fb0a7f
goutamoff7 <goutamoff7@gmail.com>
2024-03-29 21:11:56 +0530

    Demonstration of Inter Thread Communication - wait() and notify() mehod

---
+++ multiThreading/InterThreadCommunication_Demo.java
@@ -0,0 +1,79 @@
+package multiThreading;
+class Value
+{
+    int num;
+    boolean setValue = false;
+    synchronized public void set(int num)
+    {
+        while(setValue)
+        {
+            try{ wait();} catch(Exception e){ e.printStackTrace();}
+        }
+        System.out.println("Set : "+num);
+        this.num = num;
+        setValue = true;
+        notify();        
+    }
+    synchronized public void get()
+    {
+        while(!setValue)
+        {
+            try{ wait();} catch(Exception e){ e.printStackTrace();}
+        }
+        System.out.println("Get : "+num);
+        setValue=false;
+        notify();   
+    }
+}
+
+class Producer implements Runnable
+{
+    Value v;
+    public Producer(Value v)
+    {
+        this.v=v;
+        Thread t = new Thread(this,"Producer");
+        t.start();
+    }
+    public void run()
+    {
+        int i=0;
+        while(true)
+        {
+            v.set(i++);
+            try{ Thread.sleep(500);}
+            catch(Exception e) { e.printStackTrace();}            
+        }
+    }
+}
+
+class Consumer implements Runnable
+{
+    Value v;
+    public Consumer(Value v)
+    {
+        this.v=v;
+        Thread t = new Thread(this,"Consumer");
+        t.start();
+    }
+    public void run()
+    {
+        int i=0;
+        while(true)
+        {
+            v.get();
+            try{ Thread.sleep(3000);}
+            catch(Exception e) { e.printStackTrace();}
+            
+        }
+    }
+}
+class InterThreadCommunication_Demo
+{
+    public static void main(String[] args) 
+    {
+        Value v = new Value();
+        new Producer(v);
+        new Consumer(v);
+    }
+}
