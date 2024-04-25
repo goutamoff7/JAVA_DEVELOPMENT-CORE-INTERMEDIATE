8732d55047a6b34e8bc38a8bf6500a503cc0fbb1
goutamoff7 <goutamoff7@gmail.com>
2024-04-25 20:47:30 +0530

    Demonstration of Private methods in interface

---
+++ interfaceDemo/PrivateInterfaceMethods.java
@@ -0,0 +1,49 @@
+package interfaceDemo;
+interface Calculator
+{
+    default void add()
+    {
+        printInfo();
+        System.out.println(10+20);
+    }
+
+    default void sub()
+    {
+        printInfo();
+        System.out.println(10-20);
+    }
+
+    default void mul()
+    {
+        printInfo();
+        System.out.println(10*20);
+    }
+
+    default void div()
+    {
+        printInfo();
+        System.out.println(10/20);
+    }
+
+    private void printInfo()
+    {
+        System.out.println("This is an Interface Method");
+        System.out.println("This Method enhances Functionality");
+        System.out.println("This Method performs Calculation");
+    }
+}
+
+class Casio implements Calculator
+{
+
+}
+public class PrivateInterfaceMethods
+{
+    public static void main(String[] args) {
+        Casio cs = new Casio();
+        cs.add();
+        cs.sub();
+        cs.mul();
+        cs.div();
+    }
+}
