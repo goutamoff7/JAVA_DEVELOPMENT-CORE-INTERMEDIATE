17c8075fb5608c0533a742a48401fd6f139f1b8c
goutamoff7 <goutamoff7@gmail.com>
2025-01-14 22:46:21 +0530

    demonstration of forEachOrdered() method for parallelStream

--- Java8/TerminalOps.java
+++ Java8/TerminalOps.java
@@ -1,7 +1,6 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TerminalOps {
     public static void main(String[] args) {
@@ -42,6 +41,63 @@ public class TerminalOps {
         Optional<Integer> any = list.stream().findAny();
         System.out.println("Any element : " + any);
 
+        // 7. toArray()
+        Object[] array = Stream.of(1, 2, 3).toArray();
+
+        // 8. min(), max()
+        System.out.println("min : " + Stream.of(58, 46, 97, 12).min(Comparator.naturalOrder()));
+        System.out.println("max : " + Stream.of(58, 46, 97, 12).max(Comparator.naturalOrder()));
+        System.out.println("min : " + Stream.of(58, 46, 97, 12)
+                .min((o1, o2) -> o1 - o2)); // ascending order 1st value = min
+        System.out.println("max : " + Stream.of(58, 46, 97, 12)
+                .min((o1, o2) -> o2 - o1)); // descending order 1st value = max
+        // for (o1,o2) order
+        // Negative (o1 - o2 < 0): No swap (o1 is less than o2).
+        //Zero (o1 - o2 == 0): No swap (o1 is equal to o2).
+        //Positive (o1 - o2 > 0): Swap (o1 is greater than o2).
+
+        // 8. flatMap()
+        // Handles the stream of collections, list or arrays where each element is itself a collection
+        // Flatten nested structures (e.g., lists within lists) so that they can be processed
+        // as a single sequence of elements
+        // Transform and flatten elements at the same time.
+        List<List<String>> listOfList = Arrays.asList(
+                Arrays.asList("Apple", "Banana"),
+                Arrays.asList("Mango", "Guava"),
+                Arrays.asList("Pineapple", "Lemon")
+        );
+        System.out.println(listOfList);
+        // Getting Pineapple
+        System.out.println(listOfList.get(2).get(0));
+        // flatten the list and make uppercase
+        System.out.println(listOfList.stream()
+                .flatMap(x -> x.stream())
+                .map(String::toUpperCase)
+                .toList());
+        System.out.println(listOfList.stream()
+                .flatMap(Collection::stream)
+                .map(String::toUpperCase)
+                .toList());
+
+        //Example : take each word of the sentence and make single list with all uppercase letter
+        List<String> sentences = Arrays.asList(
+                "Hello World",
+                "Java Streams are powerful",
+                "flatMap is useful");
+        System.out.println(sentences);
+        List<String> list1 = sentences.stream()
+                .flatMap(sentence -> Arrays.stream(sentence.split(" ")))
+                .map(String::toUpperCase)
+                .toList();
+        System.out.println(list1);
+
+        // 9. forEachOrdered() --> used for parallel stream
+        List<Integer> list3 = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        System.out.println("forEach() with parallel stream : ");
+        list3.parallelStream().forEach(System.out::println);
+        System.out.println("forEachOrdered() with parallel stream : ");
+        list3.parallelStream().forEachOrdered(System.out::println);
+
         // Examples
         List<String> names = Arrays.asList("Anna", "Bob", "Charlie", "David");
 
@@ -58,7 +114,15 @@ public class TerminalOps {
         // Question 4: Counting occurrence of a character 'l'
         String sentence = "Hello World";
         long frequencyOfCharacter = sentence.chars().filter(x -> x == 'l').count();
-        System.out.println("Occurrence of character 'l' : "+ frequencyOfCharacter);
+        System.out.println("Occurrence of character 'l' : " + frequencyOfCharacter);
+
+        //  Streams cannot be reused after a terminal operation has been called
+        Stream<String> stream = names.stream();
+        stream.forEach(System.out::println);
+//     List<String> list2 = stream.map(String::toUpperCase).toList();
+        // Exception: stream has already been operated
+
+
 
     }
 }
