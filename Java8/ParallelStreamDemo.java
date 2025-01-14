8263746e10cfb3ed5a8ccf92f681090dc18475f0
goutamoff7 <goutamoff7@gmail.com>
2025-01-14 22:44:53 +0530

    Demonstration of AtomicInteger fulfilling the requirements of variable reassignment

--- Java8/ParallelStreamDemo.java
+++ Java8/ParallelStreamDemo.java
@@ -1,6 +1,11 @@
+import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 public class ParallelStreamDemo {
     public static void main(String[] args) {
@@ -20,25 +25,54 @@ public class ParallelStreamDemo {
         startTime = System.currentTimeMillis();
         factorialsList = list.parallelStream().map(ParallelStreamDemo::factorial).toList();
         endTime = System.currentTimeMillis();
-        System.out.println("total time required using parallelStream : "+(endTime-startTime));
+        System.out.println("total time required using parallelStream : "+(endTime-startTime)+" ms");
 
         // parallelStream are more effective for CPU-intensive or large datasets where tasks are independent
         // They may add overhead for simple tasks or small datasets
 
         //Dependent tasks example
-        // Cumulative sum
+        // Cumulative sum using stream (getting correct result)
         // [1,2,3,4,5] --> [1,3,6,10,15] 15=1+2+3+4+5
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
-        int sum=0;
-       List<Integer> cumulativeSumList = numbers.stream().map(x -> {
-            return sum = sum + x; // Variable used in lambda expression should be final or effectively final
-        }).toList();
+//        int sum=0;
+//       List<Integer> cumulativeSumList = numbers.stream().map(x -> {
+//            return  sum+= x; // Variable used in lambda expression should be final or effectively final
+//        }).toList();
+
+        //final Variable--> A variable declared as final cannot be reassigned after its initial assignment.
+
+        // Effectively Final Variable --> A variable is effectively final if it is not explicitly declared
+        // as final, but its value does not change after it is initialized.
+
+        AtomicInteger sum =new AtomicInteger(0);
+        List<Integer> cumulativeSumList = numbers.stream().map(x->sum.addAndGet(x)).toList();
+        System.out.println("Expected cumulative sum : [1, 3, 6, 10, 15]");
+        System.out.println("Result with stream "+cumulativeSumList);
+
+        // Cumulative sum using parallelStream (getting incorrect result as operation is dependent on other element)
+        AtomicInteger sum2 = new AtomicInteger(0);
+        List<Integer> cumulativeSumList2 = numbers.parallelStream().map(x->sum2.addAndGet(x)).toList();
+        System.out.println("Result with parallelStream "+cumulativeSumList2);
+
+//         sequential() --> This method is used to convert a parallel stream back to a sequential one.
+//        Why Sequential is Required:
+//        In parallel streams, the order of processing is not guaranteed, which could lead to an incorrect order when concatenating the strings.
+//        By converting the stream to sequential after the parallel processing, we ensure that the concatenation respects the original list's order, which is crucial in this case.
+        factorialsList = list.parallelStream().map(ParallelStreamDemo::factorial).sequential().toList();
     }
+
+
 
     static long factorial(int n){
         long fact = 1;
         for(int i=2;i<=n;i++)
             fact=fact*i;
+        return fact;
+    }
+    static BigInteger BigFactorial(int n){
+        BigInteger fact = new BigInteger("1");
+        for(int i=2;i<=n;i++)
+            fact=fact.multiply(new BigInteger(String.valueOf(i)));
         return fact;
     }
 }
