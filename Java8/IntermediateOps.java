f905f5d55f3e8053fcf35d161267b73d37cf0f13
goutamoff7 <goutamoff7@gmail.com>
2025-01-14 22:40:38 +0530

    adding more intermediate operation methods

--- Java8/IntermediateOps.java
+++ Java8/IntermediateOps.java
@@ -7,14 +7,14 @@ public class IntermediateOps {
         // Intermediate operations transform a stream into another stream
         // They are Lazy, meaning they don't execute until a terminal operation is invoked
 
-        // 1. filter()
+        // 1. filter() --> take Predicate as argument
         List<String> list = Arrays.asList("Laxman","Ram","Shyam","Bharat","Ram");
         Stream<String> filteredStream = list.stream().filter(x->x.endsWith("m"));
         // No Filtering occurs at this point
         long count = filteredStream.count();
         System.out.println("No of Names ends with 'm'= "+count);
 
-        // 2. map()
+        // 2. map() --> take Function as argument
         List<String> uppercaseNames = list.stream().map(String::toUpperCase).toList();
         System.out.println(uppercaseNames);
 
@@ -26,19 +26,24 @@ public class IntermediateOps {
         List<String> sortedByLength=list.stream().sorted((a, b) -> a.length() - b.length()).toList();
         System.out.println(sortedByLength);
 
-        // 4. distinct()
+        // 4. distinct() --> take unique element
         long m = list.stream().filter(x -> x.endsWith("m")).distinct().count();
         System.out.println(m);
 
-        // 5. limit()
+        // 5. limit() --> take only the mentioned nos of element
         List<Integer> list1 = Stream.iterate(1, x -> x + 1).limit(20).toList();
         System.out.println(list1);
 
-        // 6. skip()
+        // 6. skip() --> skip mentioned nos of element from first
         List<Integer> list2 = Stream.iterate(1, x -> x + 1).skip(20).limit(40).toList();
         // first 20 elements are skipped
         System.out.println(list2);
+
+        // 7. peek() -- > performs an action on each element as it is consumed
+        long count1 = Stream.iterate(1, x -> x + 1).skip(2).limit(20).peek(System.out::println).count();
+        System.out.println("No of elements: "+count1);
     }
+
 
 
 }
